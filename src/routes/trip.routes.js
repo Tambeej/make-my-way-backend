2b1559732005c2a5c1892b9bf12d15df2287fd6b
Zinger Marat <zingermarat@gmail.com>
2025-09-16 17:37:34 +0300

    feat: Implement trip management features; add save, delete, and fetch shared trips controllers

--- src/routes/trip.routes.js
+++ src/routes/trip.routes.js
@@ -3,12 +3,20 @@ import { planTripController } from "../controllers/trip.controller.js"
 import { getTripPathController } from "../controllers/getTripPath.controller.js"
 import { saveTripController } from "../controllers/saveTrip.controller.js"
 import { getTripByIdController } from "../controllers/getTripById.controller.js"
+import { getTripsOfUserController } from "../controllers/getTripOfUser.controller.js"
+import { deleteTripController } from "../controllers/deleteTrip.controller.js"
+import { getSharedWithUserTripsController } from "../controllers/getSharedWithUserTrips.controller.js"
 
 const router = Router()
 
 router.post("/planTrip", planTripController)
 router.post("/tripPath", getTripPathController)
-router.post("/save", saveTripController)
+
+router.get("/my", getTripsOfUserController)
+router.get("/sharedWithMe", getSharedWithUserTripsController)
 router.get("/:id", getTripByIdController)
+
+router.post("/save", saveTripController)
+router.delete("/:id", deleteTripController)
 
 export default router
