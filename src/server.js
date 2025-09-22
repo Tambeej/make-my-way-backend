bbb45d82863754e79b60683089bc9304e0349d92
Zinger Marat <zingermarat@gmail.com>
2025-09-22 21:07:20 +0300

    connectDB server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-import connectDB from "./config/db.js"
 
 dotenv.config()
 import connectDB from "./config/db.js"
